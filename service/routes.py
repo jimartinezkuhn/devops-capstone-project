38a7d11e8095595c5ecbc9b054a9a10439438991
jimartinezkuhn <juanignacioprograma@gmail.com>
2025-12-29 19:06:37 -0500

    Fix: Add missing blank lines to satisfy flake8 (E302)

--- service/routes.py
+++ service/routes.py
@@ -79,6 +79,7 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -93,6 +94,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -110,6 +112,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
